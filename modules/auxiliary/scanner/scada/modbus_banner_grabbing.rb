2021b1592231688a525a7c8e1c7c72d9aec0c8e3
Alan Foster <alandavid_foster@rapid7.com>
2021-03-15 19:43:53 +0000

    Add rule for redundant begin statements

--- modules/auxiliary/scanner/scada/modbus_banner_grabbing.rb
+++ modules/auxiliary/scanner/scada/modbus_banner_grabbing.rb
@@ -178,10 +178,10 @@ class MetasploitModule < Msf::Auxiliary
   end
 
   def cleanup
-    begin
-      disconnect
-    rescue StandardError
-      nil
-    end
+
+    disconnect
+  rescue StandardError
+    nil
+
   end
 end
