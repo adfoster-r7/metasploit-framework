f2f0256d874ce5da37de77e270739f83f842f4df
adfoster-r7 <alandavid_foster@rapid7.com>
2022-08-08 01:40:15 +0100

    Add tests for aliases

--- spec/lib/msf/core/data_store_spec.rb
+++ spec/lib/msf/core/data_store_spec.rb
@@ -2,12 +2,13 @@
 
 require 'spec_helper'
 
-RSpec.shared_examples "datastore" do
+RSpec.shared_examples "datastore" do |opts = {}|
   it "should have options" do
     expect(subject["foo"]).to eq "bar"
     expect(subject["fizz"]).to eq "buzz"
   end
-  it "should have case-insensitive keys" do
+
+  it "should have case-insensitive lookups" do
     # Sorted by gray code, just for fun
     expect(subject["foo"]).to eq "bar"
     expect(subject["Foo"]).to eq "bar"
@@ -18,11 +19,16 @@ RSpec.shared_examples "datastore" do
     expect(subject["FoO"]).to eq "bar"
     expect(subject["foO"]).to eq "bar"
   end
+
   context "#to_h" do
     it "should return a Hash with correct values" do
-      expect(subject.to_h).to eq({ "foo" => "bar", "fizz" => "buzz" })
+      expected_to_h = opts.fetch(:expected_to_h) do
+        { "foo" => "bar", "fizz" => "buzz" }
+      end
+      expect(subject.to_h).to eq(expected_to_h)
     end
   end
+
   context "#delete" do
     it "should delete the specified case-insensitive key" do
       expect(subject.delete("foo")).to eq "bar"
@@ -32,7 +38,6 @@ RSpec.shared_examples "datastore" do
 end
 
 RSpec.describe Msf::DataStore do
-
   describe "#import_option" do
     subject do
       s = described_class.new
@@ -82,5 +87,53 @@ RSpec.describe Msf::DataStore do
     it_behaves_like "datastore"
   end
 
+  context 'when importing options with aliases' do
+    subject do
+      s = described_class.new
+      s.import_option("foo", "bar")
+      s.import_option("fizz", "buzz")
 
+      options = Msf::OptionContainer.new(
+        [
+          Msf::OptString.new(
+            'NewOptionName',
+            [true, 'An option with a new name. Aliases ensure the old and new names are synchronized', 'default_value'],
+            aliases: ['OLD_OPTION_NAME']
+          )
+        ]
+      )
+
+      s.import_options(options)
+      s
+    end
+
+    describe '#[]' do
+      it 'should have default options' do
+        expect(subject['NewOptionName']).to eq('default_value')
+        expect(subject['OLD_OPTION_NAME']).to eq('default_value')
+      end
+
+      it 'should have case-insensitive lookups' do
+        expect(subject['NEWOPTIONNAME']).to eq('default_value')
+        expect(subject['Old_Option_Name']).to eq('default_value')
+      end
+    end
+
+    describe '#[]=' do
+      it 'should allow setting datastore values with the new option name' do
+        subject['NewOptionName'] = 'new_value_1'
+        expect(subject['NewOptionName']).to eq('new_value_1')
+        expect(subject['OLD_OPTION_NAME']).to eq('new_value_1')
+      end
+
+      it 'should allow setting datastore values with the old option name' do
+        subject['OLD_OPTION_NAME'] = 'new_value_2'
+        expect(subject['NewOptionName']).to eq('new_value_2')
+        expect(subject['OLD_OPTION_NAME']).to eq('new_value_2')
+      end
+    end
+
+    it_behaves_like "datastore",
+                    expected_to_h: { 'NewOptionName' => 'default_value', "foo" => "bar", "fizz" => "buzz" }
+  end
 end
