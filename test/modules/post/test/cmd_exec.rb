5349fbd66840fb1a843f3f2b7820ceab3d9c049b
cgranleese-r7 <Christopher_Granleese@rapid7.com>
2024-07-08 10:20:57 +0100

    Adds support to test custom payload branches

--- test/modules/post/test/cmd_exec.rb
+++ test/modules/post/test/cmd_exec.rb
@@ -21,10 +21,30 @@ class MetasploitModule < Msf::Post
     )
   end
 
+  def upload_precompiled_binaries
+    print_status 'Uploading precompiled binaries'
+    if session.platform.eql? 'linux'
+      upload_file('show_args', 'data/cmd_exec/show_args_linux')
+    end
+
+    if session.platform.eql? 'windows'
+      upload_file('show_args.exe', 'data/cmd_exec/show_args.exe')
+    end
+
+    if session.platform.eql? 'osx'
+      upload_file('show_args', 'data/cmd_exec/show_args_macos')
+    end
+
+    if session.platform.eql?('linux') || session.platform.eql?('osx')
+      chmod('show_args')
+    end
+  end
+
   def test_cmd_exec
     # we are inconsistent reporting windows session types
     windows_strings = ['windows', 'win']
     vprint_status("Starting cmd_exec tests")
+    upload_precompiled_binaries
 
     it "should return the result of echo" do
       test_string = Rex::Text.rand_text_alpha(4)
@@ -35,6 +55,29 @@ class MetasploitModule < Msf::Post
         output = cmd_exec("echo #{test_string}")
       end
       output == test_string
+    end
+
+    it 'should execute show_args_* executables and return the passed arguments' do
+      if session.platform.eql? 'windows'
+        if (session.type.eql? 'shell') || (session.type.eql?('meterpreter') && session.arch.eql?('php'))
+          output = cmd_exec('show_args.exe one two')
+        elsif session.type.eql?('meterpreter') && session.arch.eql?('python')
+          output = cmd_exec('show_args.exe', 'one two')
+        end
+        return output.rstrip == "show_args.exe\r\none\r\ntwo" unless output.nil?
+
+        output = cmd_exec('./show_args.exe one two')
+        if session.type.eql? 'powershell'
+          output.rstrip == "#{pwd}\\show_args.exe\r\none\r\ntwo"
+        elsif session.type.eql?('meterpreter') && session.arch.eql?('java')
+          output.rstrip == ".\\show_args.exe\r\none\r\ntwo"
+        else
+          output.rstrip == "./show_args.exe\r\none\r\ntwo"
+        end
+      else
+        output = cmd_exec('./show_args one two')
+        output.rstrip == "./show_args\none\ntwo"
+      end
     end
 
     # Powershell supports this, but not windows meterpreter (unsure about windows shell)
